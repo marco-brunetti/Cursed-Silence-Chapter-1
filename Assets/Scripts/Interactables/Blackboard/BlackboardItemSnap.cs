0e25524897feb39e99d8e1472ae53b3e015d86eb
Marco <marco19-96@hotmail.com>
2024-08-18 16:25:41 -0400

    Added remaining snaps

--- Assets/Scripts/Interactables/Blackboard/BlackboardItemSnap.cs
+++ Assets/Scripts/Interactables/Blackboard/BlackboardItemSnap.cs
@@ -8,23 +8,26 @@ public class BlackboardItemSnap : MonoBehaviour
     [NonSerialized] public bool Snapped;
     public BlackboardItem BlackboardItem { get; private set; }
 
+    private BlackboardController _controller;
+
     private void Awake()
     {
+        _controller = BlackboardController.Instance;
         BlackboardItem = transform.parent.GetComponent<BlackboardItem>();
     }
     private void OnTriggerEnter(Collider other)
     {
         var validSnap = other.TryGetComponent(out BlackboardItemSnap snap) && snap && snap.Id == Id;
-        var isOnBlackboard = validSnap && BlackboardController.Instance.BlackboardItems.Contains(snap.BlackboardItem.gameObject);
-        var validItem = isOnBlackboard && BlackboardItem == BlackboardController.Instance.CurrentItem && BlackboardItem.Orientation == snap.BlackboardItem.Orientation;
+        var isOnBlackboard = validSnap && _controller.BlackboardItems.Contains(snap.BlackboardItem.gameObject);
+        var validItem = isOnBlackboard && BlackboardItem == _controller.CurrentItem && BlackboardItem.Orientation == snap.BlackboardItem.Orientation;
 
         if (validItem)
         {
             Snapped = true;
             snap.Snapped = true;
 
-            print($"snapped{gameObject.name}");
-            BlackboardController.Instance.CancelHold();
+            print($"snapped {gameObject.name}");
+            _controller.CancelHold();
             var parentOffset = transform.parent.transform.position - transform.position;
             transform.parent.transform.position = snap.transform.position + parentOffset;
         }
