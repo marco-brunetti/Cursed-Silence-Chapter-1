ae736633ea276e39d2b7cc9fc573c582d776e18b
Marco <marco19-96@hotmail.com>
2024-09-11 07:57:29 -0400

    Added anchor list to decorators

--- Assets/Scripts/LevelLayout/LevelDecorator.cs
+++ Assets/Scripts/LevelLayout/LevelDecorator.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class LevelDecorator : MonoBehaviour
 {
-    [field: SerializeField] public bool Enable { get; private set; } = true;
+
+    [field: SerializeField, Header("Decorator parameters")] public bool Enable { get; private set; } = true;
     [field: SerializeField] public List<LayoutShape> Layouts { get; private set; }
     [field: SerializeField] public List<AnchorCompatibility> Anchors { get; private set; }
     [field: SerializeField] public Vector3 Position { get; private set; }
     [field: SerializeField] public Vector3 Rotation { get; private set; }
 
     [field: SerializeField] private Vector3 scale;
+
+    [field: SerializeField, Header("Item parameters")] public List<Transform> ItemAnchors {  get; private set; }
 
     [NonSerialized] public bool IsUsed;
     public Vector3 Scale 
