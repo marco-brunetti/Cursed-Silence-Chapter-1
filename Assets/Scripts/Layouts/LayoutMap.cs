7bb6c6b8b3d9402b859460abd968c0849dc611a1
Marco Brunetti <marco19-96@hotmail.com>
2024-09-17 10:59:37 -0400

    Created layouts namespace

---
+++ Assets/Scripts/Layouts/LayoutMap.cs
@@ -0,0 +1,23 @@
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Layouts
+{
+    public record LayoutMap
+    {
+        [JsonProperty("layouts")] public List<Layout> Layouts;
+    }
+
+    public record Layout
+    {
+        public bool enable;
+        public int zone;
+        public List<LayoutItem> items;
+        public List<LayoutType> nextShapes;
+    }
+
+    public record LayoutItem
+    {
+        public int id { get; set; }
+    }
+}
