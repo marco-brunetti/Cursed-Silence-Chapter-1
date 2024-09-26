939d37eb74e9a6a4a5f2965b7d3110e0840f9c6c
Marco <marco19-96@hotmail.com>
2024-09-25 23:27:14 -0400

    Added new animations to project

--- Assets/Scripts/Enemies/EnemyController.cs
+++ Assets/Scripts/Enemies/EnemyController.cs
@@ -9,6 +9,7 @@ namespace Enemies
         private static readonly int AnimatorDie = Animator.StringToHash("die");
         private static readonly int AnimatorIdle = Animator.StringToHash("idle");
         private static readonly int AnimatorAttack = Animator.StringToHash("attack");
+        private static readonly int AnimatorReactFront = Animator.StringToHash("react_front");
         [SerializeField] private EnemyData data;
         [SerializeField] private new Collider collider;
         [SerializeField] private Animator animator;
@@ -19,14 +20,16 @@ namespace Enemies
         private bool isEngaging;
         private bool canParry;
         private EnemyState currentState = EnemyState.Idle;
-        
+
         public void ActivateParry() => canParry = true;
         public void DeactivateParry() => canParry = false;
+
+        public void DeactivateReactAnimation() => animator.SetBool(AnimatorReactFront, false);
 
         private void OnEnable()
         {
             currentHealth = data.Health;
-            
+
             rightPlayerDetector.Controller = this;
             leftPlayerDetector.Controller = this;
         }
@@ -37,7 +40,14 @@ namespace Enemies
             currentHealth -= damageAmount;
             Debug.Log($"Dealing damage {damageAmount} remaining enemyhealth: {currentHealth}");
 
-            if (currentHealth <= 0) currentState = EnemyState.Dead;
+            if (currentHealth <= 0)
+            {
+                currentState = EnemyState.Dead;
+            }
+            else
+            {
+                animator.SetBool(AnimatorReactFront, true);
+            }
         }
 
         public void PlayerDetected()
@@ -67,20 +77,20 @@ namespace Enemies
             animator.SetBool(AnimatorDie, true);
             animator.SetBool(AnimatorIdle, false);
         }
-        
+
         private void Idle()
         {
             collider.enabled = true;
-            animator.SetBool(AnimatorDie, false);
-            animator.SetBool(AnimatorIdle, true);
+            /*animator.SetBool(AnimatorDie, false);
+            animator.SetBool(AnimatorIdle, true);*/
         }
 
         private void Attack()
         {
-            
+
         }
     }
-    
+
     public enum EnemyState
     {
         Idle,
