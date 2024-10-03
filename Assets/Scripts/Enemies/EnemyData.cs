691b1b63d610b3c6ae7bff7856e59376a41f46df
Marco <marco19-96@hotmail.com>
2024-10-03 03:32:43 -0400

    Started adding enemy stats

--- Assets/Scripts/Enemies/EnemyData.cs
+++ Assets/Scripts/Enemies/EnemyData.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData", order = 2)]
 public class EnemyData : ScriptableObject
 {
-    public int Health = 100;
+    [field: SerializeField, Header("Combat")] public int Health { get; private set; } = 100;
+    [field: SerializeField] public int Poise { get; private set; } = 100;
+    [field: SerializeField] public int LightAttack { get; private set; } = 10;
+    [field: SerializeField] public int LightAttackPoiseDecrement { get; private set; } = 10;
+    [field: SerializeField] public int HeavyAttack { get; private set; } = 20;
+    [field: SerializeField] public int HeavyAttackPoiseDecrement { get; private set; } = 20;
 
     [field: SerializeField, Header("Animations")] public AnimationClip[] AnimationClips {  get; private set; }
     [field: SerializeField] public RuntimeAnimatorController AnimatorController { get; private set; }
