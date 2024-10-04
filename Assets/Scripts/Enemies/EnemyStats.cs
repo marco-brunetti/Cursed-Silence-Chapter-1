2fdd47011fb333a818a05568fe936e7b1687c2bd
Marco Brunetti <marco19-96@hotmail.com>
2024-10-04 16:00:35 -0400

    Finished enemy stats test successfully

--- Assets/Scripts/Enemies/EnemyStats.cs
+++ Assets/Scripts/Enemies/EnemyStats.cs
@@ -8,8 +8,8 @@ namespace Enemies
         private int currentPoise;
         private EnemyData data;
         private Random random;
-
-        public EventHandler<UpdatedStatsEventArgs> StatsChanged;
+        
+        //public EventHandler<UpdatedStatsEventArgs> StatsChanged;
 
         public EnemyStats(EnemyData data)
         {
@@ -19,13 +19,12 @@ namespace Enemies
             random = new Random(Guid.NewGuid().GetHashCode());
         }
 
-        public EnemyState RecievedAttack(AttackedStateData stateData)
+        public EnemyState RecievedAttack(EnemyAttackedStateData stateData)
         {
-            EnemyState newState;
             switch (stateData.CurrentState)
             {
                 case EnemyState.Attack:
-                    if (stateData.IsVulnerable) return BlockState(stateData); //TakeDamageState(stateData); //Add big react
+                    if (stateData.IsVulnerable) return TakeDamageState(stateData); //Add big react
                     else return DecrementPoiseState(stateData);
                 case EnemyState.Walk:
                 case EnemyState.Idle:
@@ -35,36 +34,37 @@ namespace Enemies
             }
         }
 
-        private EnemyState BlockState(AttackedStateData stateData)
+        private EnemyState BlockState(EnemyAttackedStateData stateData)
         {
             int i = random.Next(0, 100);
-            if (i < data.BlockProbability)
-            {
-                StatsChanged?.Invoke(this, GetUpdatedStatsArgs("block", randomBlockResult: i));
-                return EnemyState.Block;
-            }
 
-            StatsChanged?.Invoke(this, GetUpdatedStatsArgs("block", randomBlockResult: i));
+            //StatsChanged?.Invoke(this, GetUpdatedStatsArgs("block", randomBlockResult: i));
+
+            if (i < data.BlockProbability) return EnemyState.Block;
+
             return TakeDamageState(stateData);
         }
 
-        private EnemyState DecrementPoiseState(AttackedStateData stateData)
+        private EnemyState DecrementPoiseState(EnemyAttackedStateData stateData)
         {
             currentPoise -= stateData.PoiseDecrement;
+
+            //StatsChanged?.Invoke(this, GetUpdatedStatsArgs("poise_decrement", poiseDecrement: stateData.PoiseDecrement));
+
             if (currentPoise <= 0)
             {
                 currentPoise = data.Poise;
-                StatsChanged?.Invoke(this, GetUpdatedStatsArgs("poise_decrement", poiseDecrement: stateData.PoiseDecrement));
                 return TakeDamageState(stateData);
             }
-
             return stateData.CurrentState;
         }
 
-        private EnemyState TakeDamageState(AttackedStateData stateData)
+        private EnemyState TakeDamageState(EnemyAttackedStateData stateData)
         {
             currentHealth -= stateData.Damage;
-            StatsChanged?.Invoke(this, GetUpdatedStatsArgs("damage", damage: stateData.Damage));
+
+            //StatsChanged?.Invoke(this, GetUpdatedStatsArgs("damage", damage: stateData.Damage));
+
             if (currentHealth <= 0) return EnemyState.Dead;
             return EnemyState.React;
         }
@@ -75,40 +75,25 @@ namespace Enemies
             switch (type)
             {
                 case "block":
-                    return new UpdatedStatsEventArgs()
-                    {
-                        Type = type,
-                        BlockProbability = data.BlockProbability,
-                        RandomBlockResult = randomBlockResult
-                    };
+                    return new UpdatedStatsEventArgs() { Type = type, RandomBlockResult = randomBlockResult };
                 case "damage":
-                    return new UpdatedStatsEventArgs()
-                    {
-                        Type = type,
-                        CurrentHealth = currentHealth,
-                        HealthDecrement = damage
-                    };
+                    return new UpdatedStatsEventArgs() { Type = type, CurrentHealth = currentHealth, HealthDecrement = damage };
                 case "poise_decrement":
-                    return new UpdatedStatsEventArgs()
-                    {
-                        Type = type,
-                        CurrentPoise = currentPoise,
-                        PoiseDecrement = poiseDecrement
-                    };
+                    return new UpdatedStatsEventArgs() { Type = type, CurrentPoise = currentPoise, PoiseDecrement = poiseDecrement };
                 default:
                     return null;
             }
         }
     }
 
-    public class AttackedStateData
+    public class EnemyAttackedStateData
     {
         public readonly EnemyState CurrentState;
         public readonly bool IsVulnerable;
         public readonly int Damage;
         public readonly int PoiseDecrement;
 
-        public AttackedStateData(EnemyState currentState, bool isVulnerable, int damage, int poiseDecrement)
+        public EnemyAttackedStateData(EnemyState currentState, bool isVulnerable, int damage, int poiseDecrement)
         {
             CurrentState = currentState;
             IsVulnerable = isVulnerable;
@@ -126,6 +111,5 @@ public class UpdatedStatsEventArgs : EventArgs
     public int CurrentPoise;
     public int HealthDecrement;
     public int PoiseDecrement;
-    public int BlockProbability;
     public int RandomBlockResult;
 }
--- Assets/Scripts/Tests/EnemyStatsTest.cs
+++ Assets/Scripts/Tests/EnemyStatsTest.cs
@@ -5,18 +5,40 @@ namespace Enemies
     public class EnemyStatsTest : MonoBehaviour
     {
         [SerializeField] private EnemyData data;
+
+        [Header("Parameters")]
+        [SerializeField] private EnemyState enemyState;
+        [SerializeField] private bool isVulnerable;
+        [SerializeField] private int damage;
+        [SerializeField] private int poiseDecrement;
+        [SerializeField] private int blockProbability;
+
+
+        [Header("Results")]
+        public int CurrentHealth;
+        public int CurrentPoise;
+        private int Damage;
+        private int PoiseDecrement;
+        private int RandomBlockResult;
+
+
+        //Block
+        [SerializeField] private int blockSuccessfulEvents;
+        [SerializeField] private int blockFailedEvents;
+        [SerializeField] private float blockedPercent;
+
         private EnemyStats stats;
 
         void Awake()
         {
             stats = new(data);
-            stats.UpdatedStats += OnStatsChanged;
+            blockProbability = data.BlockProbability;
+            //stats.StatsChanged += OnStatsChanged;
         }
 
-        // Update is called once per frame
         void Update()
         {
-            stats.RecievedAttack(new(EnemyState.Attack, true, 1, 1));
+            stats.RecievedAttack(new(enemyState, isVulnerable, damage, poiseDecrement));
         }
 
         private void OnStatsChanged(object sender, UpdatedStatsEventArgs e)
@@ -24,28 +46,20 @@ namespace Enemies
             switch (e.Type)
             {
                 case "block":
-                    return new UpdatedStatsEventArgs()
-                    {
-                        Type = type,
-                        BlockProbability = data.BlockProbability,
-                        RandomBlockResult = randomBlockResult
-                    };
+
+                    if (e.RandomBlockResult < blockProbability) blockSuccessfulEvents++;
+                    else blockFailedEvents++;
+
+                    if(blockFailedEvents > 0 && blockSuccessfulEvents > 0) blockedPercent = (float)blockSuccessfulEvents / (blockSuccessfulEvents + blockFailedEvents);
+                    break;
                 case "damage":
-                    return new UpdatedStatsEventArgs()
-                    {
-                        Type = type,
-                        CurrentHealth = currentHealth,
-                        HealthDecrement = damage
-                    };
+                    CurrentHealth = e.CurrentHealth;
+                    Damage = e.HealthDecrement;
+                    break;
                 case "poise_decrement":
-                    return new UpdatedStatsEventArgs()
-                    {
-                        Type = type,
-                        CurrentPoise = currentPoise,
-                        PoiseDecrement = poiseDecrement
-                    };
-                default:
-                    return null;
+                    CurrentPoise = e.CurrentPoise; 
+                    PoiseDecrement = e.PoiseDecrement;
+                    break;
             }
         }
     }
