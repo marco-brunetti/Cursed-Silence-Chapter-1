9694f51aae6d6d84f5712c4b4c85f277e72ecab8
Marco <marco19-96@hotmail.com>
2024-10-09 22:56:12 -0300

    Modified enemy data ranges

--- Assets/Scripts/Enemies/EnemyData.cs
+++ Assets/Scripts/Enemies/EnemyData.cs
@@ -5,27 +5,24 @@ namespace Enemies
     [CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData", order = 2)]
     public class EnemyData : ScriptableObject
     {
-
         [field: SerializeField, Header("Resistance"), Space(-5)] public int Health { get; private set; } = 100;
         [field: SerializeField, Range(0, 100)] public int Poise { get; private set; } = 50;
-        
-        [field: SerializeField, Range(0, 50)] public int BlockProbability { get; private set; } = 20;
-    
-        [field: SerializeField, Header("Attack stats"), Range(0, 50)] public int LightAttack { get; private set; } = 10;
-        [field: SerializeField, Range(0, 50)] public int LightPoiseDecrement { get; private set; } = 10;
-        
-        [field: SerializeField, Range(0, 50)] public int HeavyAttack { get; private set; } = 20;
-        [field: SerializeField, Range(0, 50)] public int HeavyAttackProbability { get; private set; } = 20;
-        [field: SerializeField, Range(0, 50)] public int HeavyPoiseDecrement { get; private set; } = 20;
 
-        
-        [field: SerializeField, Range(0, 50)] public int SpecialAttack { get; private set; } = 20;
-        [field: SerializeField, Range(0, 50)] public int SpecialAttackProbability { get; private set; } = 20;
-        [field: SerializeField, Range(0, 50)] public int SpecialPoiseDecrement { get; private set; } = 20;
-        
-        
-        
-        [field: SerializeField] public LayerMask DetectionMask { get; private set; }
+        [field: SerializeField, Header("Damage"), Range(0, 50)] public int LightAttackDamage { get; private set; } = 10;
+        [field: SerializeField, Range(0, 50)] public int HeavyAttackDamage { get; private set; } = 20;
+        [field: SerializeField, Range(0, 50)] public int SpecialAttackDamage { get; private set; } = 50;
+
+        [field: SerializeField, Header("Poise Decrement"), Range(0, 30)] public int LightAttackPoiseDecrement { get; private set; } = 10;
+        [field: SerializeField, Range(0, 30)] public int HeavyAttackPoiseDecrement { get; private set; } = 20;
+        [field: SerializeField, Range(0, 30)] public int SpecialAttackPoiseDecrement { get; private set; } = 20;
+
+        [field: SerializeField, Header("Probability"), Range(0, 20)] public int HeavyAttackProbability { get; private set; } = 20;
+        [field: SerializeField, Range(0, 20)] public int SpecialAttackProbability { get; private set; } = 5;
+        [field: SerializeField, Range(0, 50)] public int BlockProbability { get; private set; } = 20;
+
+
+        [field: SerializeField, Header("Player Tracking")] public LayerMask DetectionMask { get; private set; }
+
 
         [field: SerializeField, Header("Animations"), Space(10)]
         public AnimationClip IdleAnim { get; private set; }
