84c71597ceec49c479c9b386448cb4842ee6ece8
Marco Brunetti <marco19-96@hotmail.com>
2024-10-18 18:56:50 -0300

    Added basic main menu ui

---
+++ Assets/Scripts/Game/General/MainMenuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Video;
+
+namespace Game.General
+{
+    public class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private VideoPlayer videoPlayer;
+        [SerializeField] private GameObject mainMenuObject;
+    
+        private readonly WaitForSeconds initialWait = new(0.5f);
+        private WaitForSeconds clipDurationWait;
+        private VideoClip videoClip;
+
+        private void Awake()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(PlayVideoLogo());
+            videoClip = videoPlayer.clip;
+            clipDurationWait = new WaitForSeconds((float)videoClip.length);
+        }
+
+        private IEnumerator PlayVideoLogo()
+        {
+            yield return initialWait;
+            videoPlayer.Play();
+            yield return clipDurationWait;
+            mainMenuObject.SetActive(true);
+            videoPlayer.gameObject.SetActive(false);
+            
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+}
